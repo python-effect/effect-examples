677d94eab78f630876802cb0eb62aa51a4ae4343
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-11-23 11:13:56 -0600

    add a quick and simple example of using Readline in __main__

--- readline_intent.py
+++ readline_intent.py
@@ -1,5 +1,3 @@
-from six.moves import input
-
 from effect import sync_performer
 
 
@@ -14,3 +12,14 @@ class ReadLine(object):
 def perform_readline_stdin(dispatcher, readline):
     """Perform a :obj:`ReadLine` intent by reading from stdin."""
     return input(readline.prompt)
+
+
+if __name__ == '__main__':
+    from effect import Effect, TypeDispatcher, sync_perform
+    # Effect-using code:
+    effect = Effect(ReadLine("Tell me your name: ")).on(
+        success=lambda name: "Hello, {}!".format(name)
+    )
+    # Effect-dispatching code:
+    dispatcher = TypeDispatcher({ReadLine: perform_readline_stdin})
+    print("result of effect:", sync_perform(dispatcher, effect))
